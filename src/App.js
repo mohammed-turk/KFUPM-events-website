8579fed510d7e8d92bf4b480889164dc757c1ecc
Ibrahim Ahmed <ibrahimahmed@MacBook-Pro-Ibrahim.local>
2025-04-19 16:53:05 +0300

    adding AllEventList on admin and user

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import './App.css';
+import AdminAllEventList from './pages/admin/AdminAllEventList';
 import AdminClubPage from "./pages/admin/AdminClubPage";
 import AdminProfilePage from "./pages/admin/AdminProfilePage";
 import MembersList from "./pages/org/MembersList";
 import LoginPage from "./pages/shared/LoginPage";
-
-
+// Make sure to import UserAllEventList component from the correct path
+// Assuming it's in the pages/user directory:
+import UserAllEventList from "./pages/user/UserAllEventList";
 
 function App() {
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                <Route path="/" element={<Navigate to="/login" />} />  //added new path before login to solve an error
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/adminClubs" element={<AdminClubPage />} />
-                <Route path="/members" element={<MembersList />} />
-                <Route path="/adminProf" element={<AdminProfilePage />} />
+                    <Route path="/" element={<Navigate to="/login" />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/adminClubs" element={<AdminClubPage />} />
+                    <Route path="/members" element={<MembersList />} />
+                    <Route path="/adminProf" element={<AdminProfilePage />} />
+                    <Route path="/admin/AdminAllEventList" element={<AdminAllEventList />} />
+                    <Route path="/user/UserAllEventList" element={<UserAllEventList />} />
                 </Routes>
             </BrowserRouter>
         </div>
@@ -24,4 +28,3 @@ function App() {
 }
 
 export default App;
-
