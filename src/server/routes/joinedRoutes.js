9a1838bda190726c5971d50587af30a16b850670
Hasan-Al-Harbi <S202163510@kfupm.edu.sa>
2025-05-08 16:25:02 +0300

    adding function to post useFollowClub

--- src/server/routes/joinedRoutes.js
+++ src/server/routes/joinedRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {getJoinedClubs} = require("../controllers/joinedController");
+const {getJoinedClubs, joinClub} = require("../controllers/joinedController");
 
 router.get("/", async (req, res) => {
     try {
@@ -14,6 +14,23 @@ router.get("/", async (req, res) => {
     } catch (error) {
         console.error("Error fetching events:", error);
         res.status(500).json({ error: "Server error" });
+    }
+});
+
+router.post("/joinclub", async (req, res) => {
+    try {
+        const { clubID, userID } = req.body;
+
+        const joined = await joinClub(userID, clubID);
+
+        res.status(201).json({
+            success: true,
+            message: "User successfully joined the club.",
+            data: joined
+        });
+    } catch (error) {
+        console.error("Error joining club:", error);
+        res.status(500).json({ error: error.message });
     }
 });
 
